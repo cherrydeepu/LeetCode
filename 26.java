445ef9934b34c8e907a9906689415afb08a0e458
Rishitha <trishithapandu2218@gmail.com>
2026-07-05 16:45:24 +0100

    Add files via upload

--- 26.java
+++ 26.java
@@ -1,12 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i=0;
-        for(int j=1;j<nums.length;j++){
+        if(nums.length==0){
+            return 0;
+        }
+        int j=0;
+        for(int i=1;i<nums.length;i++){
             if(nums[i]!=nums[j]){
-                i++;
-                nums[i]=nums[j];
+                j++;
+                nums[j]=nums[i];
             }
         }
-        return i+1;
+        return j+1;
     }
 }
